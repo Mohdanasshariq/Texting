c5b615c0b15737e2e03a81a561e7d8269b97fdee
Mohdanasshariq <mohdanasshariq@gmail.com>
2025-03-16 03:53:23 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 //import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 //import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
-function App() {
+import Alert from './components/Alert';
+export default function App() {
+  const[mode,setMode]=useState('light')
+  const[alert,setAlert]=useState(null)
+  const showAlert =(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type,
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+
+  }
+  
+ const toggleMode=()=>{
+  if(mode==='light'){
+    setMode('dark');
+    document.body.style.backgroundColor='#042743'
+    showAlert("Dark mode has been enabled","success")
+  }
+  else {
+    setMode('light')
+    document.body.style.backgroundColor='white'
+    showAlert("Light mode has been enabled","success")
+  }
+
+  }
   return (
     <>
    
-<Navbar title='Textutils'/>
+<Navbar title="Textutils" mode={mode}  toggleMode={toggleMode}/>
+<Alert alert={alert}></Alert>
 <div className="container">
-<Textform heading = "Write the text to Anaylse"/>
+<Textform showAlert={showAlert} heading = "Write the text to Anaylse" mode={mode}/>
 {/*<About/>*/}
 </div>
 
     </>
   );
-}
-
-export default App;
+  }
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -6,10 +6,17 @@ export default function Textform(props) {
     const handleUpClick=()=>{
       let newText=text.toUpperCase()
       setText(newText)
+      props.showAlert("Converted to Upper Case","success")
     }
     const handleLowClick=()=>{
       let newText=text.toLowerCase()
       setText(newText)
+      props.showAlert("Converted to Lower Case","success")
+    }
+    const handleClick=()=>{
+      let newText=("")
+      setText(newText)
+      props.showAlert("Text Cleared","success")
     }
     const handleOnChange =(event)=>{
       setText(event.target.value)
@@ -19,17 +26,18 @@ export default function Textform(props) {
     
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color:props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
      <div className="mb-3">
      
-     <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+     <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
      </div>
      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Upper Case</button>
 
      <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to lower Case</button>
+     <button className="btn btn-primary mx-2" onClick={handleClick}>Clear Text</button>
     </div>
-    <div className='conatainer m-3'>
+    <div className='conatainer m-3' style={{color:props.mode==='dark'?'white':'black'}}>
       <h2>Your Text Summary</h2>
       <p>{text.split(" ").length} words and {text.length} characters</p>
     </div>
